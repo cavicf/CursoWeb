40ce49ca1d586565d0702c957154474d4bf0b130
Camily <cavicfina@gmail.com>
2025-02-25 11:09:05 -0300

    Atribuições

---
+++ JavaScriptBasico/Atribuições/script.js
@@ -0,0 +1,26 @@
+// =
+// +=
+// -=
+// *=
+// /=
+// %=
+
+// As operações podem ser escritas da seguinte maneira para aumentar a praticidade e leitura do código:
+var a = 30;
+
+a+=2; //Isso é o mesmo que falar que a = a + 2, ou seja, o igual nessas operações diz respeito a própria varável.
+
+console.log(a); //Imprime 32
+
+//Isso funciona para todos os operadores
+a-=2; //Mesmo que a = a-2 
+console.log(a); //Imprime 30
+
+a*=2; //Mesmo que a = a * 2
+console.log(a); //Imprime 60
+
+a/=2; //Mesmo que a = a/2
+console.log(a); //Imprime 30
+
+a%=4; //Mesmo que a = a % 4 
+console.log(a); //Imprime 2
