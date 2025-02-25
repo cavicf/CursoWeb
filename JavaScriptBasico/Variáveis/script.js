5819d232b259b8a56f1dbdd25c639c2ce57db94a
Camily <cavicfina@gmail.com>
2025-02-24 22:49:32 -0300

    Variáveis

---
+++ JavaScriptBasico/Variáveis/script.js
@@ -0,0 +1,13 @@
+// As variáveis são espaços alocados na memória para guardarmos diferentes tipos de dados. Devemos dar um nome significativo a elas, fazendo referencia ao dado que irá receber. Para declarar variáveis no javascript fazemos da seguinte forma:
+
+var caixa = 'sapato';
+
+// nesse caso criamos uma variavel chamada caixa e nela guardamos a palavra sapato e em seguida imprimimos o conteudo desta variavel a chamando na função console.log 
+
+console.log(caixa)
+
+// As variaveis como o proprio nome ja diz, podem ser modificadas a qualquer instante como vemos abaixo, onde o dado sapato foi substituido por salto 
+
+caixa = 'salto';
+
+console.log(caixa);
