e0242072e2acf74ee2103e5d40d7706abea51cfd
Camily <cavicfina@gmail.com>
2025-03-17 19:20:29 -0300

    Const, let e var

--- JavaScriptBasico/Const, Let e var/script.js
+++ JavaScriptBasico/Const, Let e var/script.js
@@ -1,5 +1,7 @@
-var numero1 = 4 //ja conhecemos 
-let numero2 = 5
+var numero1 = 4 //ja conhecemos, é global e pode ser alterada a qualquer momento no seu código
+
+let numero2 = 5 // o let pode ser alterado assim como variáveis globais mas somente dentro do bloco de código no qual foi criado, respeitando as limitações de bloco
+
 const numero3 = 6 //constantes uma vez atribuidam, não pode ser reatribuída, mas isso não quer dizer que o valor não possa mudar
 numero3 = 7// isso por exemplo não pode ser feito
 //Mas se tivermos um array ou um objeto como constante por exemplo, contanto que o seus endereços de memória não mudem, seus valores podem mudar
