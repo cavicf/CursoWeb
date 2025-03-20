4dc97ce2fe8912a1e860f40099ee9b9370d78447
Camily <cavicfina@gmail.com>
2025-03-20 10:06:05 -0300

    Window

---
+++ JSIntermediario/window/script.js
@@ -0,0 +1,7 @@
+//O window é um dos objetos mais importantes em JS, possuindo varios métodos, como o console.log e o alert que usamos
+//Não é necessário acessar seus métodos atraves do objeto window
+window.console.log('Teste') //se inspecionarmos os dois jeitos de declarar o console.log
+console.log('Teste') // veremos que os dois são o mesmo objeto
+window.alert('teste') // assim como o alert
+alert('teste') //é o mesmo objeto 
+//Os métodos do window são bem importantes para exibir pop-ups pro usuário, ou seja, mostram mensagens importantes que o usuário deve ver como avisos ou instruções
