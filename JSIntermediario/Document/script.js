c91c01a778d6cd656b2e015e426bacbf1dfeb8e2
Camily <cavicfina@gmail.com>
2025-03-20 10:12:06 -0300

    Document

---
+++ JSIntermediario/Document/script.js
@@ -0,0 +1,3 @@
+//Outra propriedade muito importante do objeto window é o document. Com ele conseguimos acessar nosso arquivo html dentro do js
+console.log(document) // ao inspecionar esse elemento veremos que se trata exatamente do nosso arquivo html
+//Assim jacomeçamos a ter uma ideia de como linkar nossos arquivos html e js
